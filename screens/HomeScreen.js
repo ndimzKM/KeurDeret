dc71bd2b533710ced13b909809fc06449c211365
sundiata keita <sundiatakeita@Suaibus-MacBook-Air.local>
2022-05-13 12:24:45 +0000

    HomeScreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Wellcome to home page</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
