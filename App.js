856875cee9a2682af981a3d8d730eb6014ef896f
Pho <phonum5@gmail.com>
2022-05-13 12:01:12 +0000

    small changes

--- App.js
+++ App.js
@@ -3,17 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-      <Text style={{ fontSize: 22, color: "#111" }}>
-        KeurDeret - Saving Lives
-      </Text>
-
-      <Text style={{ fontSize: 12, color: "#ddd" }}>Changes made by: Pho</Text>
-=======
-      <View>
-        <Text>Text in two Views</Text>
-      </View>
->>>>>>> 624de2a3adc3e03caf41786e2acd6e49a0a4f019
+      <Text>It's not easy like that deh</Text>
     </View>
   );
 }
