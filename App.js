450e56295bbf432efbcb400248946565704966ac
Alieu Ndimbalane <alieundimbalane@gmail.com>
2022-05-18 11:33:31 +0000

    Screens to screens

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>It's not easy like that deh</Text>
+      <Text>It is not easy like that deh</Text>
     </View>
   );
 }
