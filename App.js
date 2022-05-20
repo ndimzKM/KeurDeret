684ad73dcb39b0949423c19eb3920dbc1f47009e
Pho <phonum5@gmail.com>
2022-05-20 18:42:18 +0000

    add navigation component and screens

--- App.js
+++ App.js
@@ -5,12 +5,21 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import * as eva from "@eva-design/eva";
 import { ApplicationProvider } from "@ui-kitten/components";
 
 // local imports
 import rootReducer from "./redux/reducers";
 import Test from "./screens/Test";
+import Register from "./screens/Register";
+import DashBoard from "./screens/DashBoard";
+import Profile from "./screens/Profile";
+import Notification from "./screens/Notification";
+import Login from "./screens/Login";
+import Settings from "./screens/Settings";
 
 // store
 const store = legacy_createStore(
@@ -18,16 +27,28 @@ const store = legacy_createStore(
   composeWithDevTools(applyMiddleware(thunk))
 );
 
+// stack navigator and screen
+const { Navigator, Screen } = createNativeStackNavigator();
+
 export default function App() {
   return (
     <Provider store={store}>
       <ApplicationProvider {...eva} theme={eva.dark}>
-        <SafeAreaView style={{flex: 1, paddingTop: Platform.OS === 'android' ? 22 : 0}}>
-          <Test />
+        <SafeAreaView
+          style={{ flex: 1, paddingTop: Platform.OS === "android" ? 22 : 0 }}
+        >
+          <NavigationContainer>
+            <Navigator initialRouteName="Test">
+              <Screen name="Test" component={Test} />
+              <Screen name="Login" component={Login} />
+              <Screen name="Notification" component={Notification} />
+              <Screen name="Profile" component={Profile} />
+              <Screen name="DashBoard" component={DashBoard} />
+              <Screen name="Register" component={Register} />
+              <Screen name="Settings" component={Settings} />
+            </Navigator>
+          </NavigationContainer>
         </SafeAreaView>
-        {/* <View style={styles.container}>
-        <Text>It is not easy like that deh</Text>
-      </View> */}
       </ApplicationProvider>
     </Provider>
   );
