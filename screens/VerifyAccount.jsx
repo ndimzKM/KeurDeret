17f608dca9ee733d48092cbadffe2887ef4b1977
Aminah <Aminahjallowmj@gmail.com>
2022-06-22 11:45:23 -1200

    a new branch

---
+++ screens/VerifyAccount.jsx
@@ -0,0 +1,32 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+const VerifyAccount = () => {
+  return (
+    <View>
+      <Image
+        source={require("./assets/verify.png")}
+        style={{ width: 100, height: 100, }}
+      />
+      <Text>Verification</Text>
+      <Text>You will get a OTP vai SMS</Text>
+      <TouchableOpacity style={{borderTopWidth: 0.2,borderBottomWidth: 0.2,borderRightWidth: 0.2,borderLeftWidth: 0.2}}>
+        <Text style={{textAlign: "center"}}>hooo</Text>
+      </TouchableOpacity>
+      {/* GG */}
+      <TouchableOpacity style={{borderTopWidth: 0.2,borderBottomWidth: 0.2,borderRightWidth: 0.2,borderLeftWidth: 0.2}}>
+        <Text style={{textAlign: "center",}}>Verify</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default VerifyAccount;
